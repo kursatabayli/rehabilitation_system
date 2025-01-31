b5677df4a3a640140065cdac96e8d2712dd60dd6
Kürşat Abaylı <141150625+kursatabayli@users.noreply.github.com>
2025-01-31 22:23:41 +0300

    Update _Script.php

--- Admin/Views/Shared/_Script.php
+++ Admin/Views/Shared/_Script.php
@@ -1,7 +1,7 @@
-<script src="/assets/js/jquery-3.7.1.min.js"></script>
-<script src="/assets/js/bootstrap.min.js"></script>
-<script src="/assets/js/jquery.dataTables.min.js"></script>
-<script src="/assets/js/select2.min.js"></script>
-<script src="/assets/js/all.min.js"></script>
-<script src="/assets/js/sweetalert2.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+<script src="https://cdn.datatables.net/1.13.7/js/jquery.dataTables.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.1.0/js/select2.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/js/all.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert2/11.10.5/sweetalert2.all.min.js"></script>
 <script src="/assets/js/script.js"></script>
