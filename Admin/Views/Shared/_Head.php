185d083afb2a2ae58d1a9dac5270e4c4a5976737
Kürşat Abaylı <141150625+kursatabayli@users.noreply.github.com>
2025-01-31 22:21:59 +0300

    Update _Head.php

--- Admin/Views/Shared/_Head.php
+++ Admin/Views/Shared/_Head.php
@@ -3,10 +3,11 @@
   <meta name="viewport" content="width=1024">
   <title>Admin Paneli - <?= htmlspecialchars($title, ENT_QUOTES, 'UTF-8'); ?></title>
   <link rel="icon" type="image/x-icon" href="/assets/icons/admin.ico">
-  <link rel="stylesheet" href="/assets/css/all.min.css">
-  <link rel="stylesheet" href="/assets/css/bootstrap.min.css">
-  <link rel="stylesheet" href="/assets/css/jquery.dataTables.min.css">
-  <link rel="stylesheet" href="/assets/css/select2.min.css" />
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.5.1/css/all.min.css">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://cdn.datatables.net/1.13.7/css/jquery.dataTables.min.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.1.0/css/select2.min.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/sweetalert2/11.10.5/sweetalert2.min.css">
   <link rel="stylesheet" href="/assets/css/style.css">
-  <link rel="stylesheet" href="/assets/css/sweetalert2.min.css">
+
 </head>
